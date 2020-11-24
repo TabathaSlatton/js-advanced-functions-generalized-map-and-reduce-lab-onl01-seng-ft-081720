8d9c4d8e3e8de32674f37d72045b7f1ad48f2b56
Tabatha Slatton <valbatha@gmail.com>
2020-11-23 18:06:03 -0700

    commit tree 26e9e9585e9bb18a31b3d6a1f885c8a75270bc8d

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Add your functions here
+const map = (arr, func) => arr.map(e => func(e))
+
+const reduce = (sourceArray, func, startingPoint) => {
+    let r = (!!startingPoint) ? startingPoint : sourceArray[0]
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < sourceArray.length; i++) {
+      r = func(sourceArray[i], r)
+    }
+    return r;
+  }
